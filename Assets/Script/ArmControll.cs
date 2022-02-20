619459031029dc76f868a2f78e245ccf83f98e44
jessicafeng825 <jessicafengforever@gmail.com>
2022-02-19 20:47:30 -0500

    add new change with the arm/

--- Assets/Script/ArmControll.cs
+++ Assets/Script/ArmControll.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class ArmControll : MonoBehaviour
 {
     public GameObject target;
+    //public GameObject targetForDownArm;
     public BodySourceView2 bodysource;
     Vector3 righthandfirst;
+    //Vector2 lefthandfirst;
     Vector3 righthandupdate;
-    Vector3 targetstart;
+    //Vector3 leftandupdate;
+    Vector3 targetstartTop;
+    //Vector3 targetstartDown;
     bool isfirstframe = true;
 
     // time change smooth
@@ -22,7 +26,7 @@ public class ArmControll : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetstartTop = target.transform.localPosition;
     }
 
     // Update is called once per frame
@@ -34,23 +38,56 @@ public class ArmControll : MonoBehaviour
             if (isfirstframe)
             {
                 isfirstframe = false;
-                righthandfirst = bodysource.RightHand.localPosition;
-                targetstart = target.transform.localPosition;
+                if (target.tag == "Top")
+                {
+                    righthandfirst = bodysource.RightHand.localPosition;
+                }
+                else if (target.tag == "Down")
+                {
+                    righthandfirst = bodysource.LeftHand.localPosition;
+                }
+                   
+                    targetstartTop = target.transform.localPosition;
+                
             }
-            righthandupdate.y = Mathf.SmoothDamp(righthandupdate.y,bodysource.RightHand.localPosition.y - righthandfirst.y,ref yVelocity, smoothTime); //change overtime and just change one pivot
+            if (target.tag == "Top")
+            {
+                righthandupdate.y = Mathf.SmoothDamp(righthandupdate.y, bodysource.RightHand.localPosition.y - righthandfirst.y, ref yVelocity, smoothTime); //change overtime and just change one pivot
+            }
+            else if (target.tag == "Down")
+            {
+                righthandupdate.y = Mathf.SmoothDamp(righthandupdate.y, bodysource.LeftHand.localPosition.y - righthandfirst.y, ref yVelocity, smoothTime);
+            }
+            
+            
+            
             changeposition();
+        }
+        else if(bodysource.istrackedone)
+        {
+            target.transform.localPosition = targetstartTop;
+            isfirstframe = true;
         }
             
     }
     void changeposition()
     {
-        Debug.Log(bodysource.RightHand.localPosition);
+       // Debug.Log(bodysource.RightHand.localPosition);
         //Debug.Log(righthandfirst);
         //Debug.Log(targetstart);
-        Debug.Log(target);
-        Debug.Log(righthandupdate);
+       // Debug.Log(target);
+       // Debug.Log(righthandupdate);
+
+        if(target.tag == "Top")
+        {
+            target.transform.localPosition = targetstartTop + 2 * righthandupdate;
+
+        }
+        else if(target.tag == "Down")
+        {
+            target.transform.localPosition = targetstartTop - 2 * righthandupdate; 
+        }
         
-        target.transform.localPosition = targetstart + 2 * righthandupdate;
         
         //start from the top 
         //target.transform.localPosition = targetstart - 2 * righthandupdate; 
